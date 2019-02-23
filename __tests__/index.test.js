3643249913a3a7769cfcb98fcd2ca1d79d64504e
danielconde <danielconde9@gmail.com>
2019-02-23 12:30:56 +0000

    feat: working in swapping the original handler with the compat

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,15 @@
+const fs = require("fs");
 const ServerlessNextJsPlugin = require("../index");
 const createHttpServerLambdaCompatHandlers = require("../lib/createHttpServerLambdaCompatHandlers");
 
+jest.mock("fs");
 jest.mock("../lib/createHttpServerLambdaCompatHandlers");
 
 describe("ServerlessNextJsPlugin", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe("#constructor", () => {
     it("should hook to before:package:createDeploymentArtifacts", () => {
       const plugin = new ServerlessNextJsPlugin({}, {});
@@ -18,11 +24,91 @@ describe("ServerlessNextJsPlugin", () => {
 
   describe("#beforeCreateDeploymentArtifacts", () => {
     it("should call createHttpServerLambdaCompatHandlers with nextjs page handlers", () => {
+      expect.assertions(1);
+
       createHttpServerLambdaCompatHandlers.mockResolvedValueOnce([
         ".next/serverless/pages/home.compat.render",
         ".next/serverless/pages/home.about.render"
       ]);
 
+      const plugin = new ServerlessNextJsPlugin({
+        pluginManager: {
+          run: () => {}
+        },
+        service: {
+          functions: {
+            "home-page": { handler: ".next/serverless/pages/home.render" },
+            "about-page": { handler: ".next/serverless/pages/about.render" }
+          }
+        }
+      });
+
+      return plugin.beforeCreateDeploymentArtifacts().then(() => {
+        expect(createHttpServerLambdaCompatHandlers).toBeCalledWith({
+          "home-page": ".next/serverless/pages/home.js",
+          "about-page": ".next/serverless/pages/about.js"
+        });
+      });
+    });
+
+    it("should call createHttpServerLambdaCompatHandlers without non nextjs page handlers", () => {
+      expect.assertions(1);
+
+      createHttpServerLambdaCompatHandlers.mockResolvedValueOnce([]);
+
+      const plugin = new ServerlessNextJsPlugin({
+        pluginManager: {
+          run: () => {}
+        },
+        service: {
+          functions: {
+            foo: { handler: "path/to/foo.bar" },
+            baz: { handler: "path/to/baz.bar" }
+          }
+        }
+      });
+
+      return plugin.beforeCreateDeploymentArtifacts().then(() => {
+        expect(createHttpServerLambdaCompatHandlers).toBeCalledWith({});
+      });
+    });
+
+    it("should return the result of createHttpServerLambdaCompatHandlers", () => {
+      expect.assertions(1);
+
+      const pluginManagerRunMock = jest.fn();
+
+      createHttpServerLambdaCompatHandlers.mockResolvedValueOnce(
+        Promise.resolve([
+          "next/serverless/pages/home.compat.js",
+          "next/serverless/pages/about.compat.js"
+        ])
+      );
+
+      const plugin = new ServerlessNextJsPlugin({
+        pluginManager: {
+          run: pluginManagerRunMock
+        },
+        service: {
+          functions: {
+            "home-page": { handler: ".next/serverless/pages/home.render" },
+            "about-page": { handler: ".next/serverless/pages/about.render" }
+          }
+        }
+      });
+
+      return plugin.beforeCreateDeploymentArtifacts().then(result => {
+        expect(result).toEqual([
+          "next/serverless/pages/home.compat.js",
+          "next/serverless/pages/about.compat.js"
+        ]);
+      });
+    });
+  });
+
+  describe("#swapHandlers", () => {
+    it("should rename next handler files and append .original to them", () => {
+      fs.rename.mockImplementation((fileName, newFileName, cb) => cb(null, ""));
       const plugin = new ServerlessNextJsPlugin({
         service: {
           functions: {
@@ -32,48 +118,55 @@ describe("ServerlessNextJsPlugin", () => {
         }
       });
 
-      plugin.beforeCreateDeploymentArtifacts();
-
-      expect(createHttpServerLambdaCompatHandlers).toBeCalledWith({
-        "home-page": ".next/serverless/pages/home.js",
-        "about-page": ".next/serverless/pages/about.js"
-      });
+      return plugin
+        .swapHandlers({
+          "home-page": ".next/serverless/pages/home.compat.js",
+          "about-page": ".next/serverless/pages/about.compat.js"
+        })
+        .then(() => {
+          expect(fs.rename).toBeCalledWith(
+            ".next/serverless/pages/home.js",
+            ".next/serverless/pages/home.original.js",
+            expect.any(Function)
+          );
+          expect(fs.rename).toBeCalledWith(
+            ".next/serverless/pages/about.js",
+            ".next/serverless/pages/about.original.js",
+            expect.any(Function)
+          );
+        });
     });
 
-    it("should call createHttpServerLambdaCompatHandlers without non nextjs page handlers", () => {
-      createHttpServerLambdaCompatHandlers.mockResolvedValueOnce([]);
+    it("should set compat handler files as the main handlers", () => {
+      expect.assertions(2);
 
+      fs.rename.mockImplementation((fileName, newFileName, cb) => cb(null, ""));
       const plugin = new ServerlessNextJsPlugin({
         service: {
           functions: {
-            foo: { handler: "path/to/foo.bar" },
-            baz: { handler: "path/to/baz.bar" }
+            "home-page": { handler: ".next/serverless/pages/home.render" },
+            "about-page": { handler: ".next/serverless/pages/about.render" }
           }
         }
       });
 
-      plugin.beforeCreateDeploymentArtifacts();
-
-      expect(createHttpServerLambdaCompatHandlers).toBeCalledWith({});
-    });
-
-    it("should return with createHttpServerLambdaCompatHandlers return value", () => {
-      createHttpServerLambdaCompatHandlers.mockResolvedValueOnce(
-        Promise.resolve([])
-      );
-
-      const plugin = new ServerlessNextJsPlugin({
-        service: {
-          functions: {
-            foo: { handler: "path/to/foo.bar" },
-            baz: { handler: "path/to/baz.bar" }
-          }
-        }
-      });
-
-      return plugin.beforeCreateDeploymentArtifacts().then(result => {
-        expect(result).toEqual([]);
-      });
+      return plugin
+        .swapHandlers({
+          "home-page": ".next/serverless/pages/home.compat.js",
+          "about-page": ".next/serverless/pages/about.compat.js"
+        })
+        .then(() => {
+          expect(fs.rename).toBeCalledWith(
+            ".next/serverless/pages/home.compat.js",
+            ".next/serverless/pages/home.js",
+            expect.any(Function)
+          );
+          expect(fs.rename).toBeCalledWith(
+            ".next/serverless/pages/about.compat.js",
+            ".next/serverless/pages/about.js",
+            expect.any(Function)
+          );
+        });
     });
   });
 });
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const path = require("path");
+const { rename } = require("fs");
+const { promisify } = require("util");
 const createHttpServerLambdaCompatHandlers = require("./lib/createHttpServerLambdaCompatHandlers");
 
-class ServerlessPlugin {
+const renameAsync = promisify(rename);
+
+class ServerlessNextJsPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
@@ -14,16 +18,18 @@ class ServerlessPlugin {
       this
     );
 
+    this.swapHandlers = this.swapHandlers.bind(this);
+
     this.hooks = {
       "before:package:createDeploymentArtifacts": this
         .beforeCreateDeploymentArtifacts
     };
   }
 
-  beforeCreateDeploymentArtifacts() {
+  getNextFunctionHandlerPathsMap() {
     const functions = this.serverless.service.functions;
 
-    const jsHandlerPaths = Object.keys(functions)
+    const functionJsHandlerMap = Object.keys(functions)
       .filter(f => functions[f].handler.includes(".next/serverless/pages"))
       .reduce((acc, f) => {
         const handlerPath = functions[f].handler;
@@ -35,8 +41,34 @@ class ServerlessPlugin {
         return acc;
       }, {});
 
-    return createHttpServerLambdaCompatHandlers(jsHandlerPaths);
+    return functionJsHandlerMap;
+  }
+
+  beforeCreateDeploymentArtifacts() {
+    const functionHandlerPathMap = this.getNextFunctionHandlerPathsMap();
+    return createHttpServerLambdaCompatHandlers(functionHandlerPathMap);
+  }
+
+  swapHandlers(compatHandlerPathMap) {
+    const functionHandlerPathMap = this.getNextFunctionHandlerPathsMap();
+
+    const promises = Object.entries(compatHandlerPathMap).map(
+      ([f, compatHandlerPath]) => {
+        const originalHandlerPath = functionHandlerPathMap[f];
+        const dirname = path.dirname(originalHandlerPath);
+        const basename = path.basename(originalHandlerPath, ".js");
+        const originalRenamed = path.join(dirname, `${basename}.original.js`);
+
+        // .next/serverless/page.js -> .next/serverless/page.original.js
+        return renameAsync(originalHandlerPath, originalRenamed).then(() => {
+          // .next/serverless/page.compat.js -> .next/serverless/page.js
+          return renameAsync(compatHandlerPath, originalHandlerPath);
+        });
+      }
+    );
+
+    return Promise.all(promises);
   }
 }
 
-module.exports = ServerlessPlugin;
+module.exports = ServerlessNextJsPlugin;
