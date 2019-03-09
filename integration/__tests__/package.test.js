f839137c5229bb4b5f17b5e8b7d86e4c03c36931
danielconde <danielconde9@gmail.com>
2019-03-09 18:33:00 +0000

    test: integration test for zipped artifact

--- integration/__tests__/package.test.js
+++ integration/__tests__/package.test.js
@@ -1,64 +1,87 @@
 const path = require("path");
+const AdmZip = require("adm-zip");
 const packageTestService = require("../../utils/test/packageTestService");
 
-describe("Package Tests", () => {
-  let cloudFormationTemplateUpdate;
-  let resources;
+const appServerlessDir = "../basic-app/.serverless";
 
+describe("Package Tests", () => {
   beforeAll(() => {
     process.chdir(path.join(__dirname, "../basic-app"));
     packageTestService();
-    cloudFormationTemplateUpdate = require("../basic-app/.serverless/cloudformation-template-update-stack.json");
-    resources = cloudFormationTemplateUpdate.Resources;
   });
 
-  describe("Lambdas", () => {
-    let homePageLambdaFunction;
-    let aboutPageLambdaFunction;
+  describe("CloudFormationTemplateUpdate", () => {
+    let cloudFormationTemplateUpdate;
+    let resources;
 
     beforeAll(() => {
-      homePageLambdaFunction = resources.HomePageLambdaFunction;
-      aboutPageLambdaFunction = resources.AboutPageLambdaFunction;
+      cloudFormationTemplateUpdate = require(`${appServerlessDir}/cloudformation-template-update-stack.json`);
+      resources = cloudFormationTemplateUpdate.Resources;
     });
 
-    it("should create AWS Lambda resources for each page", () => {
-      expect(homePageLambdaFunction).toBeDefined();
-      expect(homePageLambdaFunction.Type).toEqual("AWS::Lambda::Function");
+    describe("Lambdas", () => {
+      let homePageLambdaFunction;
+      let aboutPageLambdaFunction;
 
-      expect(aboutPageLambdaFunction).toBeDefined();
-      expect(aboutPageLambdaFunction.Type).toEqual("AWS::Lambda::Function");
+      beforeAll(() => {
+        homePageLambdaFunction = resources.HomePageLambdaFunction;
+        aboutPageLambdaFunction = resources.AboutPageLambdaFunction;
+      });
+
+      it("should create AWS Lambda resources for each page", () => {
+        expect(homePageLambdaFunction).toBeDefined();
+        expect(homePageLambdaFunction.Type).toEqual("AWS::Lambda::Function");
+
+        expect(aboutPageLambdaFunction).toBeDefined();
+        expect(aboutPageLambdaFunction.Type).toEqual("AWS::Lambda::Function");
+      });
+
+      it("should have correct handlers", () => {
+        expect(homePageLambdaFunction.Properties.Handler).toEqual(
+          "sls-next-build/home.render"
+        );
+        expect(aboutPageLambdaFunction.Properties.Handler).toEqual(
+          "sls-next-build/about.render"
+        );
+      });
     });
 
-    it("should have correct handlers", () => {
-      expect(homePageLambdaFunction.Properties.Handler).toEqual(
-        "sls-next-build/home.render"
-      );
-      expect(aboutPageLambdaFunction.Properties.Handler).toEqual(
-        "sls-next-build/about.render"
-      );
+    describe("API Gateway", () => {
+      let apiGWHomePageResource;
+      let apiGWAboutPageResource;
+
+      beforeAll(() => {
+        apiGWHomePageResource = resources.ApiGatewayResourceHome;
+        apiGWAboutPageResource = resources.ApiGatewayResourceAbout;
+      });
+
+      it("should create api gateway resources", () => {
+        expect(apiGWHomePageResource).toBeDefined();
+        expect(apiGWAboutPageResource).toBeDefined();
+
+        expect(apiGWHomePageResource.Type).toEqual("AWS::ApiGateway::Resource");
+        expect(apiGWAboutPageResource.Type).toEqual(
+          "AWS::ApiGateway::Resource"
+        );
+      });
+
+      it("should have correct URI paths", () => {
+        expect(apiGWHomePageResource.Properties.PathPart).toEqual("home");
+        expect(apiGWAboutPageResource.Properties.PathPart).toEqual("about");
+      });
     });
   });
 
-  describe("API Gateway", () => {
-    let apiGWHomePageResource;
-    let apiGWAboutPageResource;
+  describe("ZipArtifacts", () => {
+    it("should have the compiled page files inside the artifact", () => {
+      const zip = new AdmZip(`${appServerlessDir}/basic-app.zip`);
+      const zipEntries = zip.getEntries();
+      const entryNames = zipEntries.map(ze => ze.entryName);
 
-    beforeAll(() => {
-      apiGWHomePageResource = resources.ApiGatewayResourceHome;
-      apiGWAboutPageResource = resources.ApiGatewayResourceAbout;
-    });
-
-    it("should create api gateway resources", () => {
-      expect(apiGWHomePageResource).toBeDefined();
-      expect(apiGWAboutPageResource).toBeDefined();
-
-      expect(apiGWHomePageResource.Type).toEqual("AWS::ApiGateway::Resource");
-      expect(apiGWAboutPageResource.Type).toEqual("AWS::ApiGateway::Resource");
-    });
-
-    it("should have correct URI paths", () => {
-      expect(apiGWHomePageResource.Properties.PathPart).toEqual("home");
-      expect(apiGWAboutPageResource.Properties.PathPart).toEqual("about");
+      expect(entryNames).toContain("sls-next-build/home.js");
+      expect(entryNames).toContain("sls-next-build/about.js");
+      expect(entryNames).toContain("sls-next-build/home.original.js");
+      expect(entryNames).toContain("sls-next-build/about.original.js");
     });
   });
 });
