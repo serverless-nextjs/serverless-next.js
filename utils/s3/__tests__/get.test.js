c3ed464c0ab1ae612a4791288810f3df1d5a6290
Daniel Conde <daniel.conde@bbc.co.uk>
2019-04-25 13:01:34 +0100

    refactor: remove superfluous describe

--- utils/s3/__tests__/get.test.js
+++ utils/s3/__tests__/get.test.js
@@ -15,69 +15,67 @@ describe("s3 get", () => {
     jest.clearAllMocks();
   });
 
-  describe("When key is not in cache", () => {
-    it("should call listObjects with bucket and prefix", () => {
-      expect.assertions(1);
+  it("should call listObjects with bucket and prefix", () => {
+    expect.assertions(1);
 
-      const bucket = "my-bucket";
+    const bucket = "my-bucket";
 
-      return get("/path/to/smile.jpg", bucket).then(() => {
+    return get("/path/to/smile.jpg", bucket).then(() => {
+      expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
+        Bucket: bucket,
+        Prefix: "/path/to"
+      });
+    });
+  });
+
+  it("should not listObjects if key already in cache", () => {
+    expect.assertions(2);
+
+    const bucket = "my-bucket";
+    const key = "/path/to/ironman.jpg";
+
+    awsProvider.mockResolvedValueOnce({
+      Contents: [
+        {
+          Key: key
+        }
+      ]
+    });
+
+    return get(key, bucket)
+      .then(() => get(key, bucket))
+      .then(() => {
+        expect(awsProvider).toBeCalledTimes(1);
         expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
           Bucket: bucket,
           Prefix: "/path/to"
         });
       });
+  });
+
+  it("should handle paginated response", () => {
+    expect.assertions(3);
+
+    const key = "/path/to/batman.jpg";
+    const bucket = "my-bucket";
+    const continuationToken = "123";
+
+    awsProvider.mockResolvedValueOnce({
+      IsTruncated: true,
+      Contents: [{ Key: "/path/to/bar.jpg" }],
+      NextContinuationToken: continuationToken
     });
 
-    it("should not listObjects if key already in cache", () => {
-      expect.assertions(2);
-
-      const bucket = "my-bucket";
-      const key = "/path/to/ironman.jpg";
-
-      awsProvider.mockResolvedValueOnce({
-        Contents: [
-          {
-            Key: key
-          }
-        ]
+    return get(key, bucket).then(() => {
+      expect(awsProvider).toBeCalledTimes(2);
+      expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
+        Bucket: bucket,
+        Prefix: "/path/to"
       });
-
-      return get(key, bucket)
-        .then(() => get(key, bucket))
-        .then(() => {
-          expect(awsProvider).toBeCalledTimes(1);
-          expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
-            Bucket: bucket,
-            Prefix: "/path/to"
-          });
-        });
-    });
-
-    it("should handle handle paginated response", () => {
-      expect.assertions(3);
-
-      const key = "/path/to/batman.jpg";
-      const bucket = "my-bucket";
-      const continuationToken = "123";
-
-      awsProvider.mockResolvedValueOnce({
-        IsTruncated: true,
-        Contents: [{ Key: "/path/to/bar.jpg" }],
-        NextContinuationToken: continuationToken
-      });
-
-      return get(key, bucket).then(() => {
-        expect(awsProvider).toBeCalledTimes(2);
-        expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
-          Bucket: bucket,
-          Prefix: "/path/to"
-        });
-        expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
-          Bucket: bucket,
-          Prefix: "/path/to",
-          ContinuationToken: continuationToken
-        });
+      expect(awsProvider).toBeCalledWith("S3", "listObjectsV2", {
+        Bucket: bucket,
+        Prefix: "/path/to",
+        ContinuationToken: continuationToken
       });
     });
   });
