7eed38fe59b32ed0acf5912f01208aeb0af16efd
danielconde <danielconde9@gmail.com>
2019-05-08 20:34:08 +0100

    test: fix issue breaking int. tests

--- utils/yml/load.js
+++ utils/yml/load.js
@@ -1,15 +1,12 @@
-const path = require("path");
 const fse = require("fs-extra");
 const yaml = require("js-yaml");
 const cfSchema = require("./cfSchema");
 
-const load = async ymlRelativePath => {
-  const fullPath = path.resolve(__dirname, ymlRelativePath);
-
-  const ymlStr = await fse.readFile(fullPath, "utf-8");
+const load = async ymlFullPath => {
+  const ymlStr = await fse.readFile(ymlFullPath, "utf-8");
 
   return yaml.safeLoad(ymlStr, {
-    fullPath,
+    ymlFullPath,
     schema: cfSchema
   });
 };
