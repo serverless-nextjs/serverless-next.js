0045872564d8fb4c72da0db525651a8c10c4df98
mike-suggitt <mikessuggitt@gmail.com>
2019-11-28 14:59:23 +0000

    - Move config processing into hooks. This follows serverless best practice and allows for variable interpolation to happen before execution.  - Updated unit tests accordingly

--- packages/serverless-nextjs-plugin/index.js
+++ packages/serverless-nextjs-plugin/index.js
@@ -14,29 +14,50 @@ class ServerlessNextJsPlugin {
     this.options = options;
     this.commands = {};
 
+    this.build = build.bind(this);
+    this.addCustomStackResources = addCustomStackResources.bind(this);
+    this.checkForChanges = checkForChanges.bind(this);
+    this.uploadStaticAssets = uploadStaticAssets.bind(this);
+
+    this.hooks = {
+      "before:offline:start": this.hookWrapper.bind(this, this.build),
+      "before:package:initialize": this.hookWrapper.bind(this, this.build),
+      "before:deploy:function:initialize": this.hookWrapper.bind(
+        this,
+        this.build
+      ),
+      "before:aws:package:finalize:mergeCustomProviderResources": this.hookWrapper.bind(
+        this,
+        this.addCustomStackResources
+      ),
+      "after:package:createDeploymentArtifacts": this.hookWrapper.bind(
+        this,
+        this.removePluginBuildDir
+      ),
+      "after:aws:deploy:deploy:checkForChanges": this.hookWrapper.bind(
+        this,
+        this.checkForChanges
+      ),
+      "after:aws:deploy:deploy:uploadArtifacts": this.hookWrapper.bind(
+        this,
+        this.uploadStaticAssets
+      ),
+      "after:aws:info:displayStackOutputs": this.hookWrapper.bind(
+        this,
+        this.printStackOutput
+      )
+    };
+  }
+
+  async hookWrapper(lifecycleFunc) {
+    this.initializeVariables();
+    return await lifecycleFunc.call(this);
+  }
+
+  initializeVariables() {
     this.provider = this.serverless.getProvider("aws");
     this.providerRequest = this.provider.request.bind(this.provider);
     this.pluginBuildDir = new PluginBuildDir(this.nextConfigDir);
-
-    this.addCustomStackResources = addCustomStackResources.bind(this);
-    this.uploadStaticAssets = uploadStaticAssets.bind(this);
-    this.build = build.bind(this);
-    this.checkForChanges = checkForChanges.bind(this);
-
-    this.printStackOutput = this.printStackOutput.bind(this);
-    this.removePluginBuildDir = this.removePluginBuildDir.bind(this);
-
-    this.hooks = {
-      "before:offline:start": this.build,
-      "before:package:initialize": this.build,
-      "before:deploy:function:initialize": this.build,
-      "before:aws:package:finalize:mergeCustomProviderResources": this
-        .addCustomStackResources,
-      "after:package:createDeploymentArtifacts": this.removePluginBuildDir,
-      "after:aws:deploy:deploy:checkForChanges": this.checkForChanges,
-      "after:aws:deploy:deploy:uploadArtifacts": this.uploadStaticAssets,
-      "after:aws:info:displayStackOutputs": this.printStackOutput
-    };
   }
 
   get nextConfigDir() {
