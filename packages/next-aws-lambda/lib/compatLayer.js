57404f1b0cb47d89cb73a4e461cccb1f893213de
a14m <abdelhalim.ahmed@protonmail.com>
2020-03-26 10:37:01 +0100

    Fix the headers to always work with lower case
    Previously the following behaviour could be observed:
    ```
    res.setHeader("X-CUSTOM", "VAL")
    res.getHeader("X-CUSTOM") => null
    res.hasHeader("X-CUSTOM") => false
    
    res.setHeader("x-custom", "val")
    res.getHeader("X-CUSTOM") => "val"
    res.hasHeader("X-CUSTOM") => true
    ```
    There is no way of accessing a header that isn't set in lower case,
    This fixes the issue by always storing the header name as lower case,
    allowing a similar behaviour to node HTTP ResponseObject behaviour
     - https://nodejs.org/api/http.html#http_request_getheader_name
     - https://nodejs.org/api/http.html#http_request_removeheader_name
     - https://nodejs.org/api/http.html#http_request_setheader_name_value

--- packages/next-aws-lambda/lib/compatLayer.js
+++ packages/next-aws-lambda/lib/compatLayer.js
@@ -86,10 +86,10 @@ const reqResMapper = (event, callback) => {
     ]);
   };
   res.setHeader = (name, value) => {
-    res.headers[name] = value;
+    res.headers[name.toLowerCase()] = value;
   };
   res.removeHeader = name => {
-    delete res.headers[name];
+    delete res.headers[name.toLowerCase()];
   };
   res.getHeader = name => {
     return res.headers[name.toLowerCase()];
