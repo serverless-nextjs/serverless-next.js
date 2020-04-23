2e121f8707cc7ba27212f1b3efa6127c4eacbb5e
danielconde <danielconde9@gmail.com>
2020-04-23 22:19:00 +0100

    chore: fix broken tests after dir rename

--- packages/serverless-component/lib/expressifyDynamicRoute.js
+++
@@ -1,9 +0,0 @@
-// converts a nextjs dynamic route /[param]/ -> /:param
-// also handles catch all routes /[...param]/ -> /:param*
-module.exports = dynamicRoute => {
-  // replace any catch all group first
-  let expressified = dynamicRoute.replace(/\[\.\.\.(.*)]$/, ":$1*");
-
-  // now replace other dynamic route groups
-  return expressified.replace(/\[(.*?)]/g, ":$1");
-};
--- packages/serverless-component/lib/pathToRegexStr.js
+++
@@ -1,6 +0,0 @@
-const pathToRegexp = require("path-to-regexp");
-
-module.exports = path =>
-  pathToRegexp(path)
-    .toString()
-    .replace(/\/(.*)\/\i/, "$1");
