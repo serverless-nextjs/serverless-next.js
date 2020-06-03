58aa682e0c7659faa575ae8a7bc447604a941743
danielconde <danielconde9@gmail.com>
2020-06-03 20:00:01 +0100

    add integration test

---
+++ packages/lambda-at-edge/tests/integration/serverless-trace-with-dynamic-import/fixture/pages/api/hello.js
@@ -0,0 +1,5 @@
+export default (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ name: 'John Doe' }))
+}
---
+++ packages/lambda-at-edge/tests/integration/serverless-trace-with-dynamic-import/serverless-trace-with-dynamic-import.test.ts
@@ -0,0 +1,60 @@
+import { remove, readdir, pathExists } from "fs-extra";
+import path from "path";
+import os from "os";
+import Builder from "../../../src/build";
+import { getNextBinary } from "../../test-utils";
+
+jest.unmock("execa");
+
+describe("Serverless Trace With Dynamic Import", () => {
+  const nextBinary = getNextBinary();
+  const fixtureDir = path.join(__dirname, "./fixture");
+  let outputDir: string;
+
+  beforeAll(async () => {
+    outputDir = os.tmpdir();
+    const builder = new Builder(fixtureDir, outputDir, {
+      cwd: fixtureDir,
+      cmd: nextBinary,
+      args: ["build"],
+      useServerlessTraceTarget: true
+    });
+
+    await builder.build();
+  });
+
+  afterAll(() => {
+    return Promise.all(
+      [".next"].map(file => remove(path.join(fixtureDir, file)))
+    );
+  });
+
+  it("copies node_modules to default lambda artefact", async () => {
+    const nodeModules = await readdir(
+      path.join(outputDir, "default-lambda/node_modules")
+    );
+    expect(nodeModules.length).toBeGreaterThan(5); // 5 is just an arbitrary number to ensure dependencies are being copied
+  });
+
+  it("copies node_modules to api lambda artefact", async () => {
+    const nodeModules = await readdir(
+      path.join(outputDir, "api-lambda/node_modules")
+    );
+    expect(nodeModules).toEqual(["next", "next-aws-cloudfront"]);
+  });
+
+  it("copies dynamic chunk to default lambda artefact", async () => {
+    const chunkFileName = (
+      await readdir(path.join(fixtureDir, ".next/serverless"))
+    ).find(file => {
+      return /^[\d]+\.+[\w,\s-]+\.(js)$/.test(file);
+    });
+
+    expect(chunkFileName).toBeDefined();
+
+    const chunkExistsInOutputBuild = await pathExists(
+      path.join(outputDir, "default-lambda", chunkFileName as string)
+    );
+    expect(chunkExistsInOutputBuild).toBe(true);
+  });
+});
