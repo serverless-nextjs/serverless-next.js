a838a643561c3bbc2f1b0e0fe734ae8dd846c8eb
Joseph Khan <joseph.khan@altayyargroup.com>
2020-07-13 22:10:50 +0400

    started to make changes

--- documentation/website/siteConfig.js
+++ documentation/website/siteConfig.js
@@ -21,8 +21,8 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'Serverless Nextjs Component', // Title for your website.
+  tagline: '',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
