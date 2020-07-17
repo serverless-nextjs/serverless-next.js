305cf78a8c22d77860cdab71b80134b50660f7d7
Joseph Khan <joseph.khan@altayyargroup.com>
2020-07-17 23:42:59 +0400

    adding doc pages

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,5 +1,6 @@
 module.exports = {
   someSidebar: {
+    Introduction: ['basics', 'motivation', 'design', 'features'],
     Docusaurus: ['doc1', 'doc2', 'doc3', 'architecture'],
     Features: ['mdx'],
   },
