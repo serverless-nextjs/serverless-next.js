81775e1fd0201caaa2a21db6c2abda21c36cbb3e
danielcondemarin <danielconde9@gmail.com>
2020-07-20 23:07:52 +0000

    Deploying to gh-pages from  @ 1fb4da0d5d7c79196cedd9dde8276a141c3b5c04 🚀

---
+++ 75424912.9739eae2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{161:function(n){n.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"nextjs","name":"nextjs","count":1,"permalink":"/blog/tags/nextjs"}')}}]);
---
+++ efc54922.1e3bbcf2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{171:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"lambda","name":"lambda","count":1,"permalink":"/blog/tags/lambda"}')}}]);
