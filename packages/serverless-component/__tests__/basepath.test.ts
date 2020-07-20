0a6815467b1aad13118cdfd9f29d11f0d9e1f0d9
Ralph Rooding <ralph@ralphrooding.com>
2020-07-20 11:14:34 +0200

    Cleaned up basepath test

--- packages/serverless-component/__tests__/basepath.test.ts
+++ packages/serverless-component/__tests__/basepath.test.ts
@@ -3,9 +3,7 @@ import fse from "fs-extra";
 import { mockS3 } from "@serverless/aws-s3";
 import { mockCloudFront } from "@sls-next/aws-cloudfront";
 import { mockLambda, mockLambdaPublish } from "@sls-next/aws-lambda";
-import mockCreateInvalidation from "@sls-next/cloudfront";
 import NextjsComponent from "../src/component";
-import { DEFAULT_LAMBDA_CODE_DIR, API_LAMBDA_CODE_DIR } from "../src/constants";
 import { cleanupFixtureDirectory } from "../src/lib/test-utils";
 
 describe("basepath tests", () => {
@@ -69,62 +67,15 @@ describe("basepath tests", () => {
   afterAll(cleanupFixtureDirectory(fixturePath));
 
   describe("cloudfront", () => {
-    it("creates distribution", () => {
-      expect(mockCloudFront).toBeCalledWith({
-        defaults: {
-          allowedHttpMethods: expect.any(Array),
-          forward: {
-            queryString: true,
-            cookies: "all"
-          },
-          ttl: 0,
-          "lambda@edge": {
-            "origin-request":
-              "arn:aws:lambda:us-east-1:123456789012:function:default-cachebehavior-func:v1"
-          },
-          compress: true
-        },
-        origins: [
-          {
-            url: "http://bucket-xyz.s3.us-east-1.amazonaws.com",
-            private: true,
-            pathPatterns: {
-              "custom_base/_next/static/*": {
-                ttl: 86400,
-                forward: {
-                  headers: "none",
-                  cookies: "none",
-                  queryString: false
-                }
-              },
-              "custom_base/_next/data/*": {
-                ttl: 0,
-                allowedHttpMethods: ["HEAD", "GET"],
-                "lambda@edge": {
-                  "origin-request":
-                    "arn:aws:lambda:us-east-1:123456789012:function:default-cachebehavior-func:v1"
-                }
-              },
-              "custom_base/static/*": {
-                ttl: 86400,
-                forward: {
-                  headers: "none",
-                  cookies: "none",
-                  queryString: false
-                }
-              },
-              "custom_base/api/*": {
-                ttl: 0,
-                "lambda@edge": {
-                  "origin-request":
-                    "arn:aws:lambda:us-east-1:123456789012:function:api-cachebehavior-func:v1"
-                },
-                allowedHttpMethods: expect.any(Array)
-              }
-            }
-          }
-        ]
-      });
+    it("adds basepath to paths", () => {
+      const cloudFrontCreateArgs = mockCloudFront.mock.calls[0][0];
+      const pathPatterns = cloudFrontCreateArgs.origins[0].pathPatterns;
+      expect(Object.keys(pathPatterns)).toEqual([
+        "custom_base/_next/static/*",
+        "custom_base/static/*",
+        "custom_base/api/*",
+        "custom_base/_next/data/*"
+      ]);
     });
   });
 });
