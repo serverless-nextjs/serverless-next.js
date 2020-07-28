01715991d9e4c468b6daec3f233bcc76a2e2514e
danielconde <danielconde9@gmail.com>
2020-07-28 21:38:55 +0100

    docs: docusaurus copy changes

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,128 +1,130 @@
 module.exports = {
-  title: 'Serverless Nextjs Component',
-  tagline: 'A zero configuration Nextjs 9.0 serverless component with full feature parity',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'serverless-nextjs', // Usually your GitHub org/user name.
-  projectName: 'Serverless NextJS Component', // Usually your repo name.
+  title: "Serverless Nextjs",
+  tagline:
+    "A zero configuration Nextjs 9.0 serverless component with full feature parity",
+  url: "https://serverless-nextjs.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "serverless-nextjs",
+  projectName: "Serverless NextJS",
   themeConfig: {
     disableDarkMode: true,
     sidebarCollapsible: false,
     announcementBar: {
-      id: 'work_in_progress', // Any value that will identify this message.
-      content:
-        'Alpha Release v1.0.0',
-      backgroundColor: 'yellow', // Defaults to `#fff`.
-      textColor: '#091E42', // Defaults to `#000`.
+      id: "work_in_progress", // Any value that will identify this message.
+      content: "Alpha Release v1.0.0",
+      backgroundColor: "yellow", // Defaults to `#fff`.
+      textColor: "#091E42" // Defaults to `#000`.
     },
     navbar: {
-      title: 'Serverless-Nextjs',
+      title: "Serverless Nextjs",
       logo: {
-        alt: 'Serverless Nextjs component',
-        src: 'img/logo.svg',
+        alt: "Serverless Nextjs",
+        src: "img/logo.svg"
       },
       links: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left"
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: "blog", label: "Blog", position: "left" },
         // {to: 'motivation', label: 'Motivation', position: 'left'},
         {
-          href: 'https://github.com/serverless-nextjs/serverless-next.js',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/serverless-nextjs/serverless-next.js",
+          label: "GitHub",
+          position: "right"
         },
         {
-          href: 'https://github.com/serverless-nextjs/serverless-next.js/tree/master/packages/serverless-component/examples',
-          label: 'Examples',
-          position: 'right',
-        },
-      ],
+          href:
+            "https://github.com/serverless-nextjs/serverless-next.js/tree/master/packages/serverless-component/examples",
+          label: "Examples",
+          position: "right"
+        }
+      ]
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Getting Started',
-              to: 'docs/installation/',
+              label: "Getting Started",
+              to: "docs/installation/"
             },
             {
-              label: 'FAQs',
-              to: 'docs/faq/',
+              label: "FAQs",
+              to: "docs/faq/"
             },
             {
-              label: 'Contributing',
-              to: '/docs/contributing/'
+              label: "Contributing",
+              to: "/docs/contributing/"
             }
-          ],
+          ]
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus"
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus"
+            }
+          ]
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog"
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/serverless-nextjs/serverless-next.js',
+              label: "GitHub",
+              href: "https://github.com/serverless-nextjs/serverless-next.js"
             },
             {
-              label: 'Examples',
-              href: 'https://github.com/serverless-nextjs/serverless-next.js/tree/master/packages/serverless-component/examples',
-            },
-          ],
-        },
+              label: "Examples",
+              href:
+                "https://github.com/serverless-nextjs/serverless-next.js/tree/master/packages/serverless-component/examples"
+            }
+          ]
+        }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Serverless-Nextjs component.`,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} Serverless-Nextjs component.`
+    }
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'basics',
-          sidebarPath: require.resolve('./sidebars.js'),
+          homePageId: "basics",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/serverless-nextjs/serverless-next.js/documentation/docs/',
+            "https://github.com/serverless-nextjs/serverless-next.js/documentation/docs/"
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/serverless-nextjs/serverless-next.js/documentation/blog/',
+            "https://github.com/serverless-nextjs/serverless-next.js/documentation/blog/"
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
 };
