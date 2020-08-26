5db33b89d7165a2898fd7c50c93ff4dbf3c224f4
Daniel Phang <dphang12@gmail.com>
2020-08-26 13:34:03 -0700

    add more test coverage

--- packages/libs/lambda-at-edge/tests/integration/with-trailing-slash-next-config/with-trailing-slash-next-config.test.ts
+++ packages/libs/lambda-at-edge/tests/integration/with-trailing-slash-next-config/with-trailing-slash-next-config.test.ts
@@ -14,54 +14,65 @@ describe("With Trailing Slash Config Build", () => {
   let defaultBuildManifest: OriginRequestDefaultHandlerManifest;
 
   describe.each`
-    fixture                          | expectedOriginalNextConfig
-    ${"fixture-next-config-as-obj"}  | ${'module.exports = { target: "serverless", trailingSlash: true };'}
-    ${"fixture-next-config-as-func"} | ${'module.exports = () => ({ target: "serverless", trailingSlash: true });'}
-  `("with fixture: $fixture", ({ fixture, expectedOriginalNextConfig }) => {
-    const fixtureDir = path.join(__dirname, `./${fixture}`);
+    fixture                                            | expectedTrailingSlash | expectedOriginalNextConfig
+    ${"fixture-next-config-as-obj"}                    | ${true}               | ${'module.exports = { target: "serverless", trailingSlash: true };'}
+    ${"fixture-next-config-as-func"}                   | ${true}               | ${'module.exports = () => ({ target: "serverless", trailingSlash: true });'}
+    ${"fixture-next-config-as-obj-no-trailing-slash"}  | ${false}              | ${'module.exports = { target: "serverless" };'}
+    ${"fixture-next-config-as-func-no-trailing-slash"} | ${false}              | ${'module.exports = () => ({ target: "serverless" });'}
+    ${"fixture-no-next-config"}                        | ${false}              | ${undefined}
+  `(
+    "with fixture: $fixture",
+    ({ fixture, expectedTrailingSlash, expectedOriginalNextConfig }) => {
+      const fixtureDir = path.join(__dirname, `./${fixture}`);
 
-    beforeAll(async () => {
-      mockDateNow = jest.spyOn(Date, "now").mockReturnValue(123);
-      outputDir = path.join(os.tmpdir(), "slsnext-test-build");
-      const builder = new Builder(fixtureDir, outputDir, {
-        cwd: fixtureDir,
-        cmd: nextBinary,
-        args: ["build"]
+      beforeAll(async () => {
+        mockDateNow = jest.spyOn(Date, "now").mockReturnValue(123);
+        outputDir = path.join(os.tmpdir(), "slsnext-test-build");
+        const builder = new Builder(fixtureDir, outputDir, {
+          cwd: fixtureDir,
+          cmd: nextBinary,
+          args: ["build"]
+        });
+
+        await builder.build(false);
+
+        defaultBuildManifest = await fse.readJSON(
+          join(outputDir, `${DEFAULT_LAMBDA_CODE_DIR}/manifest.json`)
+        );
       });
 
-      await builder.build(false);
+      afterAll(() => {
+        mockDateNow.mockRestore();
+        return Promise.all(
+          [".next", "next.config.original.123.js"].map((file) =>
+            remove(path.join(fixtureDir, file))
+          )
+        );
+      });
 
-      defaultBuildManifest = await fse.readJSON(
-        join(outputDir, `${DEFAULT_LAMBDA_CODE_DIR}/manifest.json`)
-      );
-    });
+      it("keeps user next.config.js intact after build", async () => {
+        const nextConfigPath = path.join(fixtureDir, "next.config.js");
 
-    afterAll(() => {
-      mockDateNow.mockRestore();
-      return Promise.all(
-        [".next", "next.config.original.123.js"].map((file) =>
-          remove(path.join(fixtureDir, file))
-        )
-      );
-    });
+        if (expectedOriginalNextConfig) {
+          expect(await pathExists(nextConfigPath)).toBe(true);
+          expect(
+            removeNewLineChars(await readFile(nextConfigPath, "utf-8"))
+          ).toEqual(expectedOriginalNextConfig);
+        } else {
+          expect(await pathExists(nextConfigPath)).toBe(false);
+        }
+      });
 
-    it("keeps user next.config.js intact after build", async () => {
-      const nextConfigPath = path.join(fixtureDir, "next.config.js");
-      expect(await pathExists(nextConfigPath)).toBe(true);
-      expect(
-        removeNewLineChars(await readFile(nextConfigPath, "utf-8"))
-      ).toEqual(expectedOriginalNextConfig);
-    });
+      it("cleans up temporary next.config.original.x.js generated", async () => {
+        expect(
+          await pathExists(path.join(fixtureDir, "next.config.original.123.js"))
+        ).toBe(false);
+      });
 
-    it("cleans up temporary next.config.original.x.js generated", async () => {
-      expect(
-        await pathExists(path.join(fixtureDir, "next.config.original.123.js"))
-      ).toBe(false);
-    });
-
-    it("sets trailingSlash to the value in next.config.js", async () => {
-      const { trailingSlash } = defaultBuildManifest;
-      expect(trailingSlash).toBe(true);
-    });
-  });
+      it(`sets trailingSlash in defaultBuildManifest to ${expectedTrailingSlash}`, async () => {
+        const { trailingSlash } = defaultBuildManifest;
+        expect(trailingSlash).toBe(expectedTrailingSlash);
+      });
+    }
+  );
 });
