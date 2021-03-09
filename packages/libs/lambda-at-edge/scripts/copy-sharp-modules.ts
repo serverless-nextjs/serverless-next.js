67427bfa6ef4eaf07b0302ae58b64b3eae4fc1f2
Denis Grankin <liquidautumn@gmail.com>
2021-03-09 17:49:36 +0200

    Add missing sharp dependencies

--- packages/libs/lambda-at-edge/scripts/copy-sharp-modules.ts
+++ packages/libs/lambda-at-edge/scripts/copy-sharp-modules.ts
@@ -4,5 +4,6 @@ import { join } from "path";
 // Copy sharp node_modules to the dist directory
 fse.copySync(
   join(process.cwd(), "sharp_node_modules"),
-  join(process.cwd(), "dist", "sharp_node_modules")
+  join(process.cwd(), "dist", "sharp_node_modules"),
+  { dereference: true }
 );
