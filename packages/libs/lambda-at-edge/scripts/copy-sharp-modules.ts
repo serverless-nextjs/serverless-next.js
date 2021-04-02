e977df4319bc0c9ebc008fcdbb95f5e27ed49007
Daniel Phang <dphang12@gmail.com>
2021-04-01 22:57:03 -0700

    fix(lambda-at-edge): ensure dist/sharp_node_modules empty before copying

--- packages/libs/lambda-at-edge/scripts/copy-sharp-modules.ts
+++ packages/libs/lambda-at-edge/scripts/copy-sharp-modules.ts
@@ -1,6 +1,9 @@
 import fse from "fs-extra";
 import { join } from "path";
 
+// Ensure old contents empty before copying
+fse.emptyDirSync(join(process.cwd(), "dist", "sharp_node_modules"));
+
 // Copy sharp node_modules to the dist directory
 fse.copySync(
   join(process.cwd(), "sharp_node_modules"),
