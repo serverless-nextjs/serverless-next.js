513ef037ed556e6236cf14f0956b3b6e4e5ad3ae
Daniel Phang <dphang12@gmail.com>
2021-04-23 17:06:30 -0700

    fix tests

--- packages/e2e-tests/next-app/cypress/integration/redirects.test.ts
+++ packages/e2e-tests/next-app/cypress/integration/redirects.test.ts
@@ -38,18 +38,22 @@ describe("Redirects Tests", () => {
   });
 
   describe("Non-redirect cases", () => {
-    [{ path: "//example.com/", expectedStatus: 404 }].forEach(
-      ({ path, expectedStatus }) => {
-        it(`does not redirect page ${path}`, () => {
-          // These cases should not redirect ever due to security
-          cy.ensureRouteHasStatusCode(path, expectedStatus);
-
-          cy.visit(path, { failOnStatusCode: false });
-          cy.location("pathname").should("eq", path);
-          cy.contains("Custom 404");
-        });
+    [
+      {
+        path: "//example.com/",
+        expectedPath: "/example.com",
+        expectedStatus: 404
       }
-    );
+    ].forEach(({ path, expectedPath, expectedStatus }) => {
+      it(`does not redirect page ${path}`, () => {
+        // These cases should not redirect ever due to security
+        cy.ensureRouteHasStatusCode(path, expectedStatus);
+
+        cy.visit(path, { failOnStatusCode: false });
+        cy.location("pathname").should("eq", expectedPath);
+        cy.contains("404");
+      });
+    });
   });
 
   describe("Public files always redirect to non-trailing slash path", () => {
