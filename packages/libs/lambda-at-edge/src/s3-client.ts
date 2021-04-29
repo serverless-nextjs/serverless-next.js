06791ed3a61d10ccb9befb4a170f6a6c5d81a54b
Daniel Phang <dphang12@gmail.com>
2021-04-29 00:48:52 -0700

    Delete s3-client.ts

--- packages/libs/lambda-at-edge/src/s3-client.ts
+++
@@ -1,5 +0,0 @@
-import { GetObjectCommand } from "@aws-sdk/client-s3/commands/GetObjectCommand";
-import { PutObjectCommand } from "@aws-sdk/client-s3/commands/PutObjectCommand";
-import { S3Client } from "@aws-sdk/client-s3/S3Client";
-
-export { GetObjectCommand, PutObjectCommand, S3Client };
