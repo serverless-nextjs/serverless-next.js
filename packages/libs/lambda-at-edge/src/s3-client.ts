4d0e1da82f852d2031e2ea022ab5b1713515eb84
Daniel Phang <dphang12@gmail.com>
2021-04-29 00:46:49 -0700

    fix(lambda-at-edge): fix rollup bundling for dynamic imports

---
+++ packages/libs/lambda-at-edge/src/s3-client.ts
@@ -0,0 +1,5 @@
+import { GetObjectCommand } from "@aws-sdk/client-s3/commands/GetObjectCommand";
+import { PutObjectCommand } from "@aws-sdk/client-s3/commands/PutObjectCommand";
+import { S3Client } from "@aws-sdk/client-s3/S3Client";
+
+export { GetObjectCommand, PutObjectCommand, S3Client };
