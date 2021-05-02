127698ba09aeb272b4f8b945c7c851a61187e96c
Henry <henry@kirkness.com>
2021-05-02 18:40:48 +0100

    add e2e test for isr page with getStaticPaths

---
+++ packages/e2e-tests/next-app/pages/revalidated-ssg-pages/[id].tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  GetStaticPropsResult
+} from "next";
+
+const sampleUserData = [
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Caroline" },
+  { id: 104, name: "Dave" }
+];
+
+const newUser = { id: 105, name: "Henry" };
+
+type SSGPageProps = {
+  date: string;
+  user?: typeof sampleUserData[number];
+};
+
+export default function RevalidatedSSGPage(props: SSGPageProps): JSX.Element {
+  return (
+    <React.Fragment>
+      <div>
+        <p data-cy="date-text">{props.date}</p>
+        <p data-cy="user-id-text">{props.user?.id ?? "No user found"}</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // To simulate new data becoming available in a data source we only make the
+  // `newUser` available when rendering on AWS, rather than at build time.
+  const runningOnAws = !!process.env.AWS_LAMBDA_FUNCTION_NAME;
+  const paths = sampleUserData.map((user) => ({
+    params: { id: user.id.toString() }
+  }));
+
+  if (runningOnAws) {
+    paths.push({ params: { id: newUser.id.toString() } });
+  }
+
+  return { paths, fallback: "blocking" };
+};
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>
+): Promise<GetStaticPropsResult<SSGPageProps>> {
+  const users = [...sampleUserData, newUser];
+  const user = users.find(
+    ({ id }) => context.params?.id.toString() === id.toString()
+  );
+  return {
+    revalidate: 10,
+    props: {
+      date: new Date().toJSON(),
+      user
+    }
+  };
+}
