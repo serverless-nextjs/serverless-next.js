31cc4b15eace8797c2d4f8595122a03b077bd7d1
Daniel Phang <dphang12@gmail.com>
2021-05-20 13:19:12 -0700

    fix(core): add logging of path when pathToRegexStr fails (#1082)

--- packages/libs/core/src/build/pathToRegexStr.ts
+++ packages/libs/core/src/build/pathToRegexStr.ts
@@ -1,7 +1,15 @@
 import { pathToRegexp } from "path-to-regexp";
 import { expressifyDynamicRoute } from "./expressifyDynamicRoute";
 
-export const pathToRegexStr = (path: string): string =>
-  pathToRegexp(expressifyDynamicRoute(path))
-    .toString()
-    .replace(/\/(.*)\/\i/, "$1");
+export const pathToRegexStr = (path: string): string => {
+  try {
+    return pathToRegexp(expressifyDynamicRoute(path))
+      .toString()
+      .replace(/\/(.*)\/\i/, "$1");
+  } catch (exception) {
+    console.error(
+      `Unable to convert path to regex: ${path}. Please check for any special characters.`
+    );
+    throw exception;
+  }
+};
