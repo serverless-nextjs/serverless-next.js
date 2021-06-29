077f930531d114aed4ec51971e48042b6bf43e93
Daniel Phang <dphang12@gmail.com>
2021-06-28 17:25:09 -0700

    chore: post handler size comparisons for non-fork PRs

---
+++ scripts/comment-handler-sizes.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+import fetch from "node-fetch";
+import { handlerSizeUtils } from "./handler-size-utils";
+import { Octokit } from "@octokit/rest";
+
+/**
+ * Get sizes that were calculated from existing commit SHA
+ * @param commitSha
+ */
+const getCommitSizes = async (
+  commitSha: string
+): Promise<Record<string, any>> => {
+  const SIZES_URL =
+    process.env.SIZES_URL ?? "https://d3m7nebxuhlnm8.cloudfront.net";
+
+  const response = await fetch(
+    `${SIZES_URL}/sizes-github-sha-${commitSha}.json`
+  );
+
+  return JSON.parse(response);
+};
+
+const postCommentToPullRequest = async (
+  prNumber: number,
+  comment: string
+): Promise<void> => {
+  const octokit = new Octokit({
+    auth: `token ${process.env.GITHUB_TOKEN}`
+  });
+  await octokit.issues.createComment({
+    owner: "serverless-nextjs",
+    repo: "serverless-next.js",
+    issue_number: prNumber,
+    body: comment
+  });
+};
+
+const main = async (): Promise<void> => {
+  const PULL_REQUEST_ID = parseInt(process.env.PULL_REQUEST_ID);
+  const GITHUB_BASE_SHA = process.env.GITHUB_BASE_SHA;
+  const GITHUB_NEW_SHA = process.env.GITHUB_NEW_SHA;
+
+  console.info("Get base commit's sizes");
+
+  // Get sizes from base commit
+  const baseSizes = await getCommitSizes(GITHUB_BASE_SHA);
+
+  console.info("Calculate all uncompressed handler sizes");
+
+  // Get sizes from current commit
+  const newSizes: Record<string, any> = handlerSizeUtils();
+
+  let output = `Base Handler Sizes (kB) (commit ${GITHUB_BASE_SHA}\n`;
+  output += JSON.stringify(baseSizes + "\n", null, 4);
+  output += `New Handler Sizes (kB) (commit ${GITHUB_NEW_SHA})\n`;
+  output += JSON.stringify(newSizes + "\n", null, 4);
+
+  // Post comment to pull request
+  await postCommentToPullRequest(PULL_REQUEST_ID, output);
+};
+
+main()
+  .then(() => {
+    console.info("Commented handler sizes successfully.");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(`Unhandled error: ${error}`);
+    process.exit(1);
+  });
--- scripts/upload-handler-sizes.ts
+++ scripts/upload-handler-sizes.ts
@@ -1,52 +1,7 @@
 #!/usr/bin/env node
 
-import * as path from "path";
-import * as fs from "fs";
 import * as AWS from "aws-sdk";
-
-type HandlerConfiguration = {
-  path: string;
-  handlers: Record<string, Record<string, string>>;
-};
-
-const PLATFORM_CONFIGS: Record<string, HandlerConfiguration> = {
-  "Lambda@Edge": {
-    path: "packages/libs/lambda-at-edge",
-    handlers: {
-      "Default Lambda": {
-        Standard: "dist/default-handler/standard",
-        Minified: "dist/default-handler/minified"
-      },
-      "API Lambda": {
-        Standard: "dist/api-handler/standard",
-        Minified: "dist/api-handler/minified"
-      },
-      "Image Lambda": {
-        Standard: "dist/image-handler/standard",
-        Minified: "dist/image-handler/minified"
-      },
-      "Regeneration Lambda": {
-        Standard: "dist/regeneration-handler/standard",
-        Minified: "dist/regeneration-handler/minified"
-      }
-    }
-  }
-};
-
-const getDirectorySizeInKilobytes = (
-  directoryPath: string
-): number | undefined => {
-  let size = 0;
-
-  if (fs.existsSync(directoryPath)) {
-    fs.readdirSync(directoryPath).forEach((file) => {
-      size += fs.statSync(path.join(directoryPath, file)).size;
-    });
-    return Math.round(size / 1024);
-  } else {
-    return undefined;
-  }
-};
+import { handlerSizeUtils } from "./handler-size-utils";
 
 const uploadHandlerSizesToS3 = async (
   sizes: Record<string, any>
@@ -65,22 +20,7 @@ const uploadHandlerSizesToS3 = async (
 
 console.info("Calculate all uncompressed handler sizes");
 
-const sizes: Record<string, any> = {};
-
-for (const [platform, platformConfig] of Object.entries(PLATFORM_CONFIGS)) {
-  sizes[platform] = {};
-  const packagePath = platformConfig.path;
-  for (const [handler, handlerConfig] of Object.entries(
-    platformConfig.handlers
-  )) {
-    sizes[platform][handler] = {};
-    for (const [handlerType, handlerPath] of Object.entries(handlerConfig)) {
-      const relativePath = path.join(packagePath, handlerPath);
-      sizes[platform][handler][handlerType] =
-        getDirectorySizeInKilobytes(relativePath);
-    }
-  }
-}
+const sizes: Record<string, any> = handlerSizeUtils();
 
 console.log("Calculated sizes: " + JSON.stringify(sizes, null, 2));
 
