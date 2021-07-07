69fe7395a819b40abdc6fe128e127785797a373d
Tomasz Miller <tomasz.miller@outlook.com>
2021-07-07 15:38:30 +0200

    domainName should be checked before replacing

--- packages/libs/lambda-at-edge/src/s3/s3BucketNameFromEventRequest.ts
+++ packages/libs/lambda-at-edge/src/s3/s3BucketNameFromEventRequest.ts
@@ -2,5 +2,5 @@ export const s3BucketNameFromEventRequest = (
   request: AWSLambda.CloudFrontRequest
 ): string | undefined => {
   const { region, domainName } = request.origin?.s3 || {};
-  return domainName?.replace(`.s3.${region}.amazonaws.com`, "");
+  return domainName?.includes(region) ? domainName.replace(`.s3.${region}.amazonaws.com`, "") : domainName.replace(`.s3.amazonaws.com`, "");
 };
