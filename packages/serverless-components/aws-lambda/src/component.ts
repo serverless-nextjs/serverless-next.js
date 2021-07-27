cde5af9e730f730eea6d074d440d37d2b00a379f
Daniel Phang <dphang12@gmail.com>
2021-07-27 00:59:41 -0700

    fix(aws-lambda): fix test failure due to init()

--- packages/serverless-components/aws-lambda/src/component.ts
+++ packages/serverless-components/aws-lambda/src/component.ts
@@ -49,7 +49,6 @@ class AwsLambda extends Component {
   state: any;
   save: () => void;
   load: (name: string) => any;
-  init: () => void;
 
   async default(inputs: Record<string, unknown> = {}) {
     this.context.status(`Deploying`);
