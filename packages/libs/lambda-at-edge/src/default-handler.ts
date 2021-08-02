e26f298a99c7f587bfb402dd39fe1b17ffdf5ea1
meng <depeng@jerry>
2021-08-02 23:08:19 +0800

    fix(default-handler): fix wrong response handler.

--- packages/libs/lambda-at-edge/src/default-handler.ts
+++ packages/libs/lambda-at-edge/src/default-handler.ts
@@ -104,7 +104,10 @@ const addS3HostHeader = (
 const isDataRequest = (uri: string): boolean => uri.startsWith("/_next/data");
 
 const normaliseUri = (uri: string, isS3Response?: boolean): string => {
-  // Don't remove the base path part if is from S3 response.
+  // Remove first characters when
+  // 1. not s3 response
+  // 2. has basepath property
+  // 3. uri starts with basepath
   if (!isS3Response && basePath && uri.startsWith(basePath)) {
     uri = uri.slice(basePath.length);
   }
