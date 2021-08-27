07140343e63910d3899d7d541dc91dde1a21c386
Daniel Phang <dphang12@gmail.com>
2021-08-26 23:03:41 -0700

    fixes

--- packages/libs/lambda-at-edge/rollup.config.js
+++ packages/libs/lambda-at-edge/rollup.config.js
@@ -30,7 +30,7 @@ const generateConfig = (input) => ({
     json(),
     commonjs(),
     externals({
-      exclude: ["@sls-next/next-aws-cloudfront", "@sls-next/core"]
+      exclude: ["@sls-next/next-aws-cloudfront", "@sls-next/core", "next"]
     }),
     nodeResolve(),
     typescript({
