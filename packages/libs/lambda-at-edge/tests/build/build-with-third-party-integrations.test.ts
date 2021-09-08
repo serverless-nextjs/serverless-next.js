135a9c80003db71b88daa533fa2966116f12a423
Daniel Phang <dphang12@gmail.com>
2021-09-08 12:26:55 -0700

    fix(lambda-at-edge): copy next-i18next files to regeneration handler

--- packages/libs/lambda-at-edge/tests/build/build-with-third-party-integrations.test.ts
+++ packages/libs/lambda-at-edge/tests/build/build-with-third-party-integrations.test.ts
@@ -4,7 +4,8 @@ import execa from "execa";
 import Builder, {
   DEFAULT_LAMBDA_CODE_DIR,
   API_LAMBDA_CODE_DIR,
-  IMAGE_LAMBDA_CODE_DIR
+  IMAGE_LAMBDA_CODE_DIR,
+  REGENERATION_LAMBDA_CODE_DIR
 } from "../../src/build";
 import { cleanupDir } from "../test-utils";
 import {
@@ -74,6 +75,31 @@ describe("Builder Tests (with third party integrations)", () => {
 
         const localesFiles = await fse.readdir(
           join(outputDir, `${DEFAULT_LAMBDA_CODE_DIR}`, "public", "locales")
+        );
+
+        expect(localesFiles).toEqual(expect.arrayContaining(["de", "en"]));
+      });
+    });
+
+    describe("Regeneration Handler Third Party Files", () => {
+      it("next-i18next files are copied", async () => {
+        expect(
+          await fse.pathExists(
+            join(
+              outputDir,
+              `${REGENERATION_LAMBDA_CODE_DIR}`,
+              "next-i18next.config.js"
+            )
+          )
+        ).toBe(true);
+
+        const localesFiles = await fse.readdir(
+          join(
+            outputDir,
+            `${REGENERATION_LAMBDA_CODE_DIR}`,
+            "public",
+            "locales"
+          )
         );
 
         expect(localesFiles).toEqual(expect.arrayContaining(["de", "en"]));
