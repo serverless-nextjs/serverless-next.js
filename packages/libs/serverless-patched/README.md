d7057ba966a5825f575dab01010ee9ad97ed2ff3
Daniel Phang <dphang12@gmail.com>
2021-09-09 15:16:33 -0700

    fix(nextjs-component): serverless-patched binary should include @serverless/cli and find serverless binary in closest node_modules (#1661)

---
+++ packages/libs/serverless-patched/README.md
@@ -0,0 +1,1 @@
+Patched serverless binary
--- packages/libs/serverless-patched/bin/serverless-patched.ts
+++ packages/libs/serverless-patched/bin/serverless-patched.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
+
 import { execSync } from "child_process";
+const findUpSync = require("find-up").sync;
 
 console.info("Note: running patched serverless binary.");
 
 const args = process.argv.slice(2).join(" ");
 // This executes package-local serverless which uses a patched @serverless/cli
-execSync(`${__dirname}/../../node_modules/.bin/serverless ${args}`, {
+// Try to find the closest serverless binary which should be patched
+const serverlessBin = findUpSync("node_modules/.bin/serverless", {
+  cwd: __dirname
+});
+
+execSync(`${serverlessBin} ${args}`, {
   stdio: "inherit"
 });
