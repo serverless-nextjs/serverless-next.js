27121276d2fdf17de433be16c7427c4ea9b10ce5
Daniel Phang <dphang12@gmail.com>
2021-10-16 19:03:24 -0700

    chore(lambda): add image handler (#1850)

---
+++ packages/libs/lambda/src/handlers/image-handler.ts
@@ -0,0 +1,89 @@
+// @ts-ignore
+import Manifest from "./manifest.json";
+// @ts-ignore
+import RoutesManifestJson from "./routes-manifest.json";
+import { AwsPlatformClient } from "@sls-next/aws-common";
+import { httpCompat } from "compat/apigw";
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyStructuredResultV2
+} from "aws-lambda";
+import { ImagesManifest, setCustomHeaders } from "@sls-next/core/dist/module";
+import url, { UrlWithParsedQuery } from "url";
+import { BuildManifest, RoutesManifest } from "types";
+import { imageOptimizer } from "@sls-next/core/dist/module/images";
+
+const basePath = RoutesManifestJson.basePath;
+
+const normaliseUri = (uri: string): string => {
+  if (uri.startsWith(basePath)) {
+    uri = uri.slice(basePath.length);
+  }
+
+  return uri;
+};
+
+const isImageOptimizerRequest = (uri: string): boolean =>
+  uri.startsWith("/_next/image");
+
+/**
+ * Entry point for Lambda image handling.
+ * @param event
+ */
+export const handler = async (
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyStructuredResultV2> => {
+  // Compatibility layer required to convert from Node.js req/res <-> API Gateway
+  const { req, res, responsePromise } = httpCompat(event);
+
+  const uri = normaliseUri(req.url ?? "");
+
+  // Handle image optimizer requests
+  // TODO: probably can move these to core package
+  const isImageRequest = isImageOptimizerRequest(uri);
+  if (isImageRequest) {
+    let imagesManifest: ImagesManifest | undefined;
+
+    try {
+      // @ts-ignore
+      imagesManifest = await import("./images-manifest.json");
+    } catch (error) {
+      console.warn(
+        "Images manifest not found for image optimizer request. Image optimizer will fallback to defaults."
+      );
+    }
+
+    const urlWithParsedQuery: UrlWithParsedQuery = url.parse(
+      req.url ?? "",
+      true
+    );
+
+    const manifest: BuildManifest = Manifest;
+
+    const awsPlatformClient = new AwsPlatformClient(
+      manifest.bucketName,
+      manifest.bucketRegion,
+      undefined,
+      undefined
+    );
+
+    await imageOptimizer(
+      basePath,
+      imagesManifest,
+      req,
+      res,
+      urlWithParsedQuery,
+      awsPlatformClient
+    );
+
+    const routesManifest: RoutesManifest = RoutesManifestJson;
+
+    setCustomHeaders({ res, req, responsePromise }, routesManifest);
+  } else {
+    // TODO: probably move this into the platform-agnostic handler
+    res.writeHead(404);
+    res.end();
+  }
+
+  return await responsePromise;
+};
