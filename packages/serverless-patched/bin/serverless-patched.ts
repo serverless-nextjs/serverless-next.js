b5f57cd23621ab790a16121cce8c8162c7c81077
Daniel Phang <dphang12@gmail.com>
2021-10-25 01:28:29 -0700

    chore(deps): move dependencies to correct packages, fix CI workflow on renovate branches, fix serverless-patched for windows

--- packages/serverless-patched/bin/serverless-patched.ts
+++ packages/serverless-patched/bin/serverless-patched.ts
@@ -2,16 +2,21 @@
 
 import { execSync } from "child_process";
 import { findUpSync } from "find-up";
+import path from "path";
+import { fileURLToPath } from "url";
 
 console.info("Note: running patched serverless binary.");
 
 const args = process.argv.slice(2).join(" ");
 // This executes package-local serverless which uses a patched @serverless/cli
 // Try to find the closest serverless binary which should be patched
-const __filename = new URL("", import.meta.url).pathname;
-const serverlessBin = findUpSync("node_modules/.bin/serverless", {
-  cwd: __filename
-});
+const __filename = fileURLToPath(import.meta.url);
+const serverlessBin = findUpSync(
+  path.join("node_modules", ".bin", "serverless"),
+  {
+    cwd: __filename
+  }
+);
 
 execSync(`${serverlessBin} ${args}`, {
   stdio: "inherit"
