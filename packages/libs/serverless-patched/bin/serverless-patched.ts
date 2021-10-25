fd96f534f040bb3bc8878ee45843e6e111f4ff71
Daniel Phang <dphang12@gmail.com>
2021-10-24 21:50:34 -0700

    chore(serverless-patched): update to es module

--- packages/libs/serverless-patched/bin/serverless-patched.ts
+++ packages/libs/serverless-patched/bin/serverless-patched.ts
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
 import { execSync } from "child_process";
-const findUpSync = require("find-up").sync;
+import { findUpSync } from "find-up";
 
 console.info("Note: running patched serverless binary.");
 
 const args = process.argv.slice(2).join(" ");
 // This executes package-local serverless which uses a patched @serverless/cli
 // Try to find the closest serverless binary which should be patched
-const serverlessBin = findUpSync("node_modules/.bin/serverless", {
-  cwd: __dirname
-});
+const serverlessBin = findUpSync("node_modules/.bin/serverless");
 
 execSync(`${serverlessBin} ${args}`, {
   stdio: "inherit"
