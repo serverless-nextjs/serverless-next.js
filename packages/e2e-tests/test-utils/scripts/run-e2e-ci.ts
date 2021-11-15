07beaed0e709d62181b21f2ebcb960d3e7fd8fb6
Daniel Phang <dphang12@gmail.com>
2021-11-14 23:39:11 -0800

    fix

--- packages/e2e-tests/test-utils/scripts/run-e2e-ci.ts
+++ packages/e2e-tests/test-utils/scripts/run-e2e-ci.ts
@@ -284,6 +284,10 @@ async function runEndToEndTest(): Promise<boolean> {
       `Creating deployment bucket if it doesn't exist: ${deploymentBucketName}`
     );
 
+    execSync(`aws configure list`, {
+      stdio: "inherit"
+    });
+
     execSync(`aws s3 mb s3://${deploymentBucketName} || true`, {
       stdio: "inherit"
     });
