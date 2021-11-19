debe1752e1700d85613941ad1d568971f83525ce
xiazhen <weiwensangsang@users.noreply.github.com>
2021-11-19 14:28:54 +0800

    Fix: classname not match (#29)
    * Update default-handler.ts
    
    * Update default-handler.ts

--- packages/libs/lambda-at-edge/src/default-handler.ts
+++ packages/libs/lambda-at-edge/src/default-handler.ts
@@ -372,6 +372,13 @@ export const handler = async (
 
   const tHandlerBegin = now();
 
+  if (process.env.NODE_ENV !== "development") {
+    // https://github.com/serverless-nextjs/serverless-next.js/issues/484#issuecomment-673152792
+    // eslint-disable-next-line no-eval
+    eval('process.env.NODE_ENV="production"');
+  }
+  debug(`[handler] node_env: ${process.env.NODE_ENV}`);
+
   if (event.revalidate) {
     const { domainName, region } = event.Records[0].cf.request.origin!.s3!;
     const bucketName = domainName.replace(`.s3.${region}.amazonaws.com`, "");
