23d158ad26460fba56ef527a5519ced22d551f33
Jan Stevens <JanStevens@users.noreply.github.com>
2022-03-02 12:01:32 +0100

    Fix: wrong replace causing "on" files on S3
    Fix the regex to only replace `.js` at the end of the string

--- packages/libs/lambda-at-edge/src/regeneration-handler.ts
+++ packages/libs/lambda-at-edge/src/regeneration-handler.ts
@@ -31,7 +31,7 @@ export const handler = async (event: AWSLambda.SQSEvent): Promise<void> => {
 
       const normalizedUri = decodeURI(regenerationEvent.pageS3Path)
         .replace(`static-pages/${manifest.buildId}`, "")
-        .replace(".js", "");
+        .replace(/.js$/, "");
 
       await s3StorePage({
         html,
