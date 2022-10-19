940e12fe04ca719bcd52d7493d6c193223dde9a2
Antoine Apollis <antoinea@theodo.fr>
2022-10-19 15:29:47 +0200

    feat: add locale to 404 page tests

--- packages/e2e-tests/next-app-with-locales/pages/404.tsx
+++ packages/e2e-tests/next-app-with-locales/pages/404.tsx
@@ -1,5 +1,15 @@
+import { useRouter } from "next/router";
+import React from "react";
+
 export default function Custom404() {
-  return <h1>Custom 404</h1>;
+  const { locale } = useRouter();
+
+  return (
+    <>
+      <h1>Custom 404</h1>
+      <p data-cy="locale">{locale}</p>
+    </>
+  );
 }
 
 export async function getStaticProps() {
