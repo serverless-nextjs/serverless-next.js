5753903ccd52dca30d893ebdc8283069b8676a27
xiazhen <weiwensangsang@users.noreply.github.com>
2022-11-25 17:02:50 +0800

    Update constants.ts

--- packages/libs/s3-static-assets/src/lib/constants.ts
+++ packages/libs/s3-static-assets/src/lib/constants.ts
@@ -10,5 +10,6 @@ export const DEFAULT_PUBLIC_DIR_CACHE_CONTROL =
 export const SERVER_NO_CACHE_CACHE_CONTROL_HEADER =
   "public, max-age=0, s-maxage=0, must-revalidate";
 
+//DEFAULT_PUBLIC_DIR_CACHE_REGEX
 export const DEFAULT_PUBLIC_DIR_CACHE_REGEX =
   /\.(gif|jpe?g|jp2|tiff|png|webp|bmp|svg|ico)$/i;
